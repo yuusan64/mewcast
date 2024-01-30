39d23c8bee8239adc6e390371bade58ba5e046f7
Sruthi <sruthivs64@gmail.com>
2024-01-30 23:23:56 +0530

    added css

--- index.js
+++ index.js
@@ -14,20 +14,16 @@ async function updateWeather(place) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        
+        console.log(data);
         document.getElementById('weatherCard').style.display = 'block';
         document.getElementById('locationDisplay').textContent = data.location.name;
+        document.getElementById('region').textContent = data.location.region;
         document.getElementById('temperature').textContent = data.current.temp_c;
         document.getElementById('feelsLike').textContent = data.current.feelslike_c;
         document.getElementById('humidity').textContent = data.current.humidity;
         document.getElementById('windSpeed').textContent = data.current.wind_kph;
-
-        // Update weather icon based on the returned data
-        const iconUrl = '[Path to Weather Icons]/' + data.current.condition.icon;
-        const weatherIcon = document.getElementById('weatherIcon');
-        weatherIcon.src = iconUrl;
-        weatherIcon.style.display = 'block';
-        
+       
+        document.getElementById('locationInput').value="";
         document.getElementById('weatherDescription').textContent = data.current.condition.text;
     } catch (error) {
         console.error('Error fetching weather data:', error);
